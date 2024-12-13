7bbe782d6ef0b7c612946c2a7dda4f09d4b2d847
Adrian-Mz <adjrmz23@gmail.com>
2024-12-12 22:37:21 -0500

    Creacion Header

--- Codigo/frontend/src/App.js
+++ Codigo/frontend/src/App.js
@@ -2,16 +2,31 @@ import React from 'react';
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import UsuariosPage from './pages/UsuarioPage';
 import ProductosPage from './pages/ProductsPage';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/usuarios" element={<UsuariosPage />}/>
-        <Route path="/productos" element={<ProductosPage />}/>
-      </Routes>
-    </Router>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow container mx-auto py-8">
+        <Routes>
+          {/* Rutas dinámicas */}
+          <Route path="/usuarios" element={<UsuariosPage />} />
+          <Route path="/productos" element={<ProductosPage />} />
+          <Route
+            path="/"
+            element={
+              <div className="text-center">
+                <h1 className="text-3xl font-bold">Bienvenido al E-Commerce</h1>
+              </div>
+            }
+          />
+        </Routes>
+      </main>
+    </div>
+  </Router>
   );
 }
 
