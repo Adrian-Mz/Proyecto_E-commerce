28e37cfa24368e1c866b5a93ecf4bcfeaa3bdd9e
Adrian-Mz <adjrmz23@gmail.com>
2024-12-16 16:49:10 -0500

    Cambios design AuthLayout

--- Codigo/frontend/src/components/UI/AuthLayout.js
+++ Codigo/frontend/src/components/UI/AuthLayout.js
@@ -3,7 +3,7 @@ import React from "react";
 const AuthLayout = ({ children }) => {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">{children}</div>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm bg-gray-800 rounded-lg shadow-lg p-8">{children}</div>
     </div>
   );
 };
