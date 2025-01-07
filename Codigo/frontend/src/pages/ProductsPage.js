081abf442ada35961a2fd72dccaa0e3e18c7fd68
Adrian-Mz <adjrmz23@gmail.com>
2025-01-06 19:04:20 -0500

    Cambio design ProductsPage

--- Codigo/frontend/src/pages/ProductsPage.js
+++ Codigo/frontend/src/pages/ProductsPage.js
@@ -136,12 +136,29 @@ const ProductosPage = () => {
                   <p className="text-gray-400">{producto.descripcion}</p>
                   <p className="font-bold text-blue-400">{`$${producto.precio}`}</p>
                 </div>
-                <button
-                  className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600'
-                  onClick={()=> alert('Producto agregado al carrito!')}
-                >
-                    Agregar al Carrito
-                </button>
+                {/* Botón Agregar al Carrito */}
+                <div className="col-span-2 flex justify-end">
+                    <button
+                        className="flex items-center bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+                        onClick={() => alert('Producto agregado al carrito!')}
+                    >
+                        <span className="mr-2">Agregar al Carrito</span>
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-5 h-5"
+                        >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M3 3h2l.4 2M7 13h10l3.4-6H6.4M7 13l-1.2 2M7 13l1.6-3M17 13l1.2 2M17 13l-1.6-3M5 21a2 2 0 100-4 2 2 0 000 4zm12 0a2 2 0 100-4 2 2 0 000 4z"
+                        />
+                        </svg>
+                    </button>
+                </div>
               </div>
             ))}
           </div>
