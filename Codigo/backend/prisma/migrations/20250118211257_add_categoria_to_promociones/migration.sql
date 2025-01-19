8130eb8d83da2e153c2ab32e4d40e9a48b700eba
Ksaeteros <kevin.saeteros@espoch.edu.ec>
2025-01-18 21:49:07 -0500

    Modificacion Prisma

--- Codigo/backend/prisma/migrations/20250118211257_add_categoria_to_promociones/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "promociones" ADD COLUMN     "categoriaId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "promociones" ADD CONSTRAINT "promociones_categoriaId_fkey" FOREIGN KEY ("categoriaId") REFERENCES "categorias"("id") ON DELETE SET NULL ON UPDATE CASCADE;
